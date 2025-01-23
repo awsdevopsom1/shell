10ddd4f545c344dc44ae335739799efac5725e94
om <awsdevopsom@gmail.com>
2025-01-23 19:38:20 +0530

    om

--- common.sh
+++ common.sh
@@ -2,9 +2,12 @@ log_file="/tmp/expense.log"
 colour="\e[33m"
 
 status_check() {
-    if [ $? -eq 0 ] ; then
+    if [ $? -eq 0 ]; then
       echo -e "\e[32m SUCCESS \e[0m"
     else
-       echo -e "\e[31m failure \e[0m"  
+       echo -e "\e[31m failure \e[0m" 
+
+       exit 1
+    fi    
 
 }
